bf596885a3158fde2f090f7ca04f8574d734ab69
gtz.jesus <gtz.jesus@outlook.com>
2024-01-11 15:01:42 -0700

    Figma landing page design

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,14 +1,40 @@
 import styled from 'styled-components';
 
 const StyledIntro = styled.div`
-  display: flex;
-  justify-content: center;
   background-image: url('backgrounds/millennium.png');
   background-size: cover;
-  height: 100vh;
+  color: var(--color-white);
+
+  // COVER THE FULL SCREEN OF EVERY DEVICE
+  min-height: 100vh;
+  min-width: 100vw;
 `;
+
+const Information = styled.div`
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: var(--gap-medium);
+  padding: 50% 0 0 0;
+`;
+
+const Title = styled.span`
+  font-size: var(--font-xlarge);
+`;
+const Description = styled.span`
+  font-size: var(--font-small);
+`;
+
 function Intro() {
-  return <StyledIntro></StyledIntro>;
+  return (
+    <StyledIntro>
+      <Information>
+        <Title>Discover a home that complements your lifestyle.</Title>
+        <Description>with assistance from a Millennium realtor.</Description>
+      </Information>
+    </StyledIntro>
+  );
 }
 
 export default Intro;
